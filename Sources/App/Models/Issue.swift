7d056db743a7276e26fe9c88ecf93aa5466cb52f
Vladimir Bozhenov <vladimir@bozhenov.ru>
2022-01-19 23:01:05 +0300

    add relation magazine-issues

--- Sources/App/Models/Issue.swift
+++ Sources/App/Models/Issue.swift
@@ -1,8 +1,36 @@
 //
-//  File.swift
+//  Issue.swift
 //  
 //
 //  Created by Vladimir Bozhenov on 19.01.2022.
 //
 
-import Foundation
+import Vapor
+import Fluent
+
+final class Issue: Model, Content {
+    static let schema = "issues"
+
+    @ID
+    var id: UUID?
+
+    @Field(key: "number")
+    var number: Int
+    
+    @Field(key: "year")
+    var year: Int
+    
+    @Parent(key: "magazineID")
+    var magazine: Magazine
+
+    init() {}
+
+    init(id: UUID? = nil, number: Int, year: Int, magazineID: Magazine.IDValue) {
+        self.id = id
+        self.number = number
+        self.year = year
+        self.$magazine.id = magazineID
+    }
+}
+
+
--- Sources/App/Models/Magazine.swift
+++ Sources/App/Models/Magazine.swift
@@ -1,8 +1,30 @@
 //
-//  File.swift
+//  Magazine.swift
 //  
 //
 //  Created by Vladimir Bozhenov on 19.01.2022.
 //
 
-import Foundation
+import Vapor
+import Fluent
+
+final class Magazine: Model, Content {
+    static let schema = "magazines"
+
+    @ID
+    var id: UUID?
+
+    @Field(key: "name")
+    var name: String
+    
+    @Children(for: \.$magazine)
+    var issues: [Issue]
+
+    init() {}
+
+    init(id: UUID? = nil, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
+

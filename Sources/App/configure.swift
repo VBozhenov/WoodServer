336fdcc9e1056816133fc30ec47976b2b1d15d3f
Vladimir Bozhenov <vladimir@bozhenov.ru>
2022-01-19 22:39:34 +0300

    Clean initial project

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,16 +6,14 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
-
-    app.migrations.add(CreateTodo())
-
-    app.views.use(.leaf)
-
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
-
+    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    
+    app.views.use(.leaf)
+    
+    
+    
     // register routes
     try routes(app)
 }

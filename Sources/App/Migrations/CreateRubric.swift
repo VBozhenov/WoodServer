8f66ffa6b1f1bbce8e868885943d5fd2b538e38c
Vladimir Bozhenov <vladimir@bozhenov.ru>
2022-01-20 09:16:19 +0300

    add CreateRubric

---
+++ Sources/App/Migrations/CreateRubric.swift
@@ -0,0 +1,24 @@
+//
+//  CreateRubric.swift
+//  
+//
+//  Created by Vladimir Bozhenov on 20.01.2022.
+//
+
+import Fluent
+
+struct CreateRubric: Migration {
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("rubrics")
+            .id()
+            .field("title", .string, .required)
+            .field("issueID", .uuid, .references("issues", "id"))
+            .unique(on: "title")
+            .create()
+    }
+
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("rubrics").delete()
+    }
+}
+

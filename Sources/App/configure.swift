098d1c5fee505a23ff144f33c5b53ad14e988b59
Vladimir Bozhenov <vladimir@bozhenov.ru>
2022-01-20 09:18:40 +0300

    add migrations

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,6 +9,16 @@ public func configure(_ app: Application) throws {
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
     app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    //    app.http.server.configuration.hostname = "192.168.100.7"
+    //    app.http.server.configuration.port = 8080
+    
+    app.migrations.add(CreateMagazine())
+    app.migrations.add(CreateIssue())
+    app.migrations.add(CreateRubric())
+    app.migrations.add(CreateArticle())
+
+    app.logger.logLevel = .debug
+    try app.autoMigrate().wait()
     
     app.views.use(.leaf)
     
